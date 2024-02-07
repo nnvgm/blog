db8fe33cc34800aa23439d6bc97f524cda0dda5b
Anhnguyen10-Sotatek <anh.nguyen10@sotatek.com>
2024-02-07 16:53:22 +0700

    fix: update astro-config to deploy github-pages

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,8 @@ import { expressiveCodeOptions } from "./src/site.config";
 // https://astro.build/config
 export default defineConfig({
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://astro-cactus.chriswilliams.dev/",
+	site: "https://nnvgm.github.io/",
+	base: "/blog",
 	markdown: {
 		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
 		rehypePlugins: [
